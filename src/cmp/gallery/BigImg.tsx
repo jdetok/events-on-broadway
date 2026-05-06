3933fe0db2d2056dcf9c76459dcb7b1cafeb0069
jdetok <jdeko17@gmail.com>
2026-05-06 16:03:52 -0500

    added previous and next buttons for gallery

--- src/cmp/gallery/BigImg.tsx
+++ src/cmp/gallery/BigImg.tsx
@@ -1,14 +1,19 @@
 import type { imgProps } from './Img';
 
-export default function BigImg(p: imgProps) {
+type bigImgProps = imgProps & {
+    onPrev?: () => void;
+    onNext?: () => void;
+}
+
+export default function BigImg(p: bigImgProps) {
     return (
-        // <div className="bigimg">
         <div className="bigimg">
             <div className="bigimg-inner">
                 <button className="bigimg-close" onClick={p.onClick}>x</button>
+                <button className="bigimg-prev" onClick={p.onPrev}>{`<`}</button>
+                <button className="bigimg-next" onClick={p.onNext}>{`>`}</button>
                 <img alt={p.alt} className={p.cssClass} src={p.src} />
             </div>
         </div>
-        
     )
 }
--- src/cmp/gallery/Gallery.tsx
+++ src/cmp/gallery/Gallery.tsx
@@ -2,25 +2,33 @@ import BigImg from "./BigImg";
 import Img from "./Img";
 import { useState, useEffect } from "react";
 
-const images = Object.values(import.meta.glob('../../../data/img/gallery/*.png', { eager: true }));
-
 export default function Gallery() {
     const cssClass = Gallery.name.toLowerCase();
     const [selected, setSelected] = useState<string | null>(null);
+    
+    const images = Object.values(import.meta.glob('../../../data/img/gallery/*.png', { eager: true }));
+    const currentIdx = images.findIndex((m: any) => m.default === selected);
+
+    const len = images.length;
+    const showPrev = () => setSelected((images[(currentIdx - 1 + len) % len] as any).default);
+    const showNext = () => setSelected((images[(currentIdx + 1 + len) % len] as any).default);
 
     useEffect(() => {
         const handleKeyDown = (e: KeyboardEvent) => {
             if (e.key === 'Escape') setSelected(null);
+            if (e.key === 'ArrowLeft') showPrev();
+            if (e.key === 'ArrowRight') showNext();
         };
         window.addEventListener('keydown', handleKeyDown);
-    }, []);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, [selected]);
 
     return (
         <div className={cssClass}>
             {selected && (
                 <>
                     <div className="bigimg-overlay" />
-                    <BigImg src={selected} onClick={() => setSelected(null) }/>
+                    <BigImg src={selected} onClick={() => setSelected(null)} onPrev={showPrev} onNext={showNext}/>
                 </>
             )}
             
