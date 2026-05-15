f260070195e7eb7e8a3704d57c9d383e10f0c078
jdetok <jdeko17@gmail.com>
2026-05-14 19:45:38 -0500

    quick cleanup buttonrow element

--- src/cmp/ButtonRow.tsx
+++ src/cmp/ButtonRow.tsx
@@ -6,7 +6,10 @@ export default function ButtonRow({ buttons }: { buttons: buttonData[] }) {
     return (
         <div className="btn-row">
             {buttons.map((btn, i) => (
-                <Button key={`${btn.cssClass}-${i}`} cssClass={btn.cssClass} onClick={btn.onClick as MouseEventHandler}>{btn.text}</Button>
+                <Button key={`${btn.cssClass}-${i}`}
+                    cssClass={btn.cssClass}
+                    onClick={btn.onClick as MouseEventHandler}>{btn.text}
+                </Button>
             ))}
         </div>
     )
